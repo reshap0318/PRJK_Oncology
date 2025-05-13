cd2adcbbebc939a29b1638cd5d325d945b89b50e
reshap0318 <aldo.reshap@gmail.com>
2025-05-13 15:03:30 +0700

    fix: add 'ct_scan_url' to tatalaksana_radioterapis and tatalaksana_targets in pemeriksaanPDF method

--- be/app/Http/Controllers/ExportController.php
+++ be/app/Http/Controllers/ExportController.php
@@ -17,8 +17,8 @@ class ExportController extends Controller
         $payload = (new PasienPemeriksaanService())->getById($id, 'laporan');
         $payload['tatalaksana_operasis'] = (new PemeriksaanOperasiService())->getData(['pemeriksaan_id' => $id])->get()->toArray();
         $payload['tatalaksana_kemoterapis'] = (new PemeriksaanKemoterapiService())->getData(['pemeriksaan_id' => $id])->get()->each->setAppends(['category_text'])->toArray();
-        $payload['tatalaksana_radioterapis'] = (new PemeriksaanRadioterapiService())->getData(['pemeriksaan_id' => $id])->get()->each->setAppends(['category_text'])->toArray();
-        $payload['tatalaksana_targets'] = (new PemeriksaanTerapiTargetService())->getData(['pemeriksaan_id' => $id])->get()->each->setAppends(['category_text'])->toArray();
+        $payload['tatalaksana_radioterapis'] = (new PemeriksaanRadioterapiService())->getData(['pemeriksaan_id' => $id])->get()->each->setAppends(['category_text', 'ct_scan_url'])->toArray();
+        $payload['tatalaksana_targets'] = (new PemeriksaanTerapiTargetService())->getData(['pemeriksaan_id' => $id])->get()->each->setAppends(['category_text', 'ct_scan_url'])->toArray();
         // dd($payload);
         
         $pdf = Pdf::loadView('exports.PemeriksaanPDF', ['payload' => $payload]);
